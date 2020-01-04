fae3a7e29bcf15edb65a401a69bb65613936bd66
kenbleiz <seb.jez29190@gmail.com>
2020-01-04 21:59:09 +0000

    First commit squelette script

---
+++ script_bash_squelette.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# DATE :20/12/2019
+# AUT : SJL
+# DES :
+# MAJ :
+# --------------------------------------------------------------------------
+
+#---------------------------------------------------------
+# Fonction de test des codes retour de la machine locale
+#---------------------------------------------------------
+check_return_code(){
+echo " ----------------------------------- "
+echo " PHASE :  $PHASE   RETCOD =  $RETCOD "
+echo " ----------------------------------- "
+if (( $RETCOD != 0 ))
+then
+         exit $RETCOD
+fi
+}
+
+#---------------------------------------------------------
+# Main : Début du shell
+#---------------------------------------------------------
+
+PHASE="INIT CODE RETOUR à ZERO  "
+(( RETCOD = 0 ))
+check_return_code
+
+# A utiliser et adapter si des parametres sont a passer au shell (exemple ./script.sh parametre1)
+# PHASE="test si le nombre de parametre est bon"
+# if [[ $# != 1 ]] ; then
+#    echo 'nombre de parametre invalide'
+#    exit 100
+# fi
+
+PhASE="cd /P1/env/PROD/"
+cd /P1/env/PROD/
+(( RETCOD = $RETCOD + $? ))
+check_return_code
+
+exit $RETCOD
---
+++ script_bash_squelette_ssh.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# DATE :
+# AUT :
+# DES :
+# MAJ :
+# --------------------------------------------------------------------------
+
+#---------------------------------------------------------
+# Fonction de test des codes retour de la machine locale
+#---------------------------------------------------------
+check_return_code_local(){
+    echo " ----------------------------------- "
+    echo " PHASE :  $PHASE   RETCOD =  $RETCOD "
+    echo " ----------------------------------- "
+    if (( $RETCOD != 0 ))
+    then
+        exit $RETCOD
+    fi
+}
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Main Fonction sur serveur distant SSH
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+fonction_serveur_distant_ssh(){
+
+    PHASE=" fonction_serveur_distant_ssh $MACHINE  "
+
+    ssh $MACHINE -l root <<!eof1
+
+        #---------------------------------------------------------
+        # Fonction de test des codes retour SSH
+        #---------------------------------------------------------
+        check_return_code_ssh(){
+        echo " ----------------------------------- "
+        echo " SSH_PHASE : sur $MACHINE  \$SSH_PHASE   SSH_RETCOD =  \$SSH_RETCOD "
+        echo " ----------------------------------- "
+        if (( \$SSH_RETCOD != 0 ))
+            then
+                 exit \$SSH_RETCOD
+        fi
+        }
+
+        #---------------------------------------------------------
+        # Main dans le ssh
+        #---------------------------------------------------------
+
+        SSH_PHASE=" INIT CODE RETOUR SSH à ZERO "
+        (( SSH_RETCOD = 0 ))
+        check_return_code_ssh
+
+
+
+        SSH_PHASE="cd /P1/env/PROD"
+        cd /P1/env/PROD
+        (( SSH_RETCOD = \$SSH_RETCOD + \$? ))
+        check_return_code_ssh
+
+!eof1
+
+}
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# Fin fonction_serveur_distant_ssh
+#---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------
+# Main : Début du shell
+#---------------------------------------------------------
+
+MACHINE=nom_serveur
+
+# A utiliser et adapter si des parametres sont a passer au shell (exemple .\script.sh parametre1)
+# PHASE="test si le nombre de parametre est OK"
+# if [[ $# != 1 ]] ; then
+#   echo 'nombre de parametre invalide'
+#   exit 100
+# fi
+
+PHASE="INIT CODE RETOUR à ZERO  "
+(( RETCOD = 0 ))
+check_return_code_local
+
+PHASE="fonction_serveur_distant_ssh"
+fonction_serveur_distant_ssh
+(( RETCOD = $RETCOD + $? ))
+check_return_code_local
+
+exit $RETCOD
