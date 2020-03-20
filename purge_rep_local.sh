b81b62163ca8b612119a45aadec874790aa16b05
kenbleiz <59445512+kenbleiz@users.noreply.github.com>
2020-03-20 10:19:26 +0100

    Update purge_rep_local.sh
    deplacement phase init retcod

--- purge_rep_local.sh
+++ purge_rep_local.sh
@@ -26,16 +26,15 @@ REPERTOIRE=$1
 #Nombre de jours a concerver
 NB_JOURS="+$2"
 
+PHASE="INIT CODE RETOUR à ZERO  "
+(( RETCOD = 0 ))
+check_return_code_local
+
 PHASE="test si le nombre de parametre est OK"
 if [[ $# != 2 ]] ; then
     echo 'nombre de parametres invalide'
     exit 100
 fi
-
-PHASE="INIT CODE RETOUR à ZERO  "
-(( RETCOD = 0 ))
-check_return_code_local
-
 
 PHASE="suppression des fichiers"
 find ${REPERTOIRE} -maxdepth 1 -type f -mtime ${NB_JOURS} -delete
